949355f609cac8bc34bf9988e2d744e7c6001730
josama-eng <samardzicjovana27@gmail.com>
2023-04-26 21:22:18 +0200

    try

--- src/components/payment/PaymentComponent.js
+++ src/components/payment/PaymentComponent.js
@@ -17,7 +17,7 @@ const PaymentComponent = ({ ck }) => {
     stripe.confirmPayment({
       elements,
       confirmParams: {
-        return_url: "https://donuts.netlify.app/order",
+        return_url: `${process.env.PUBLIC_URL}/order`,
       },
     });
   };
