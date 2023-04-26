8bc1e38168d5be13ccb3662e10e6d38ccf6a1c6d
josama-eng <samardzicjovana27@gmail.com>
2023-04-26 18:34:15 +0200

    fix

--- src/components/payment/PaymentComponent.js
+++ src/components/payment/PaymentComponent.js
@@ -17,7 +17,7 @@ const PaymentComponent = ({ ck }) => {
     stripe.confirmPayment({
       elements,
       confirmParams: {
-        return_url: "https://stalwart-nougat-a2bd57.netlify.apporder",
+        return_url: "https://stalwart-nougat-a2bd57.netlify.apporder/order",
       },
     });
   };
